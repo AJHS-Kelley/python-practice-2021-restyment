cb69e1f1665a01d35264dbd8e872037f3797a566
Geo P <90413348+restyment@users.noreply.github.com>
2021-10-28 14:16:30 -0400

    Update vairables_practice.py

--- .github/vairables/vairables_practice.py
+++ .github/vairables/vairables_practice.py
@@ -1,1 +1,9 @@
 #Variables Practice, George Peterson, 10/28/21 1:47pm, version 0.0
+
+# common Data Types and Variables in Python
+
+life_count = 3 #Interger value, all whole numbers, +/- and0.
+account_balance = 6666.66 # Floating Point value, +/-.
+user_name = "wakasa inaki" # String, a colloction of letters/numbers.
+
+print(life_count)
